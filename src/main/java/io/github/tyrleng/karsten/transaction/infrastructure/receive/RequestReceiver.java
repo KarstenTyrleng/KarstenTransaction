951a6929da31689ccd90c26ca585b4bb7528daad
YihReng <yihrengtan@outlook.com>
2021-04-04 11:34:05 +0800

    break into parts

--- src/main/java/io/github/tyrleng/karsten/transaction/infrastructure/receive/RequestReceiver.java
+++ src/main/java/io/github/tyrleng/karsten/transaction/infrastructure/receive/RequestReceiver.java
@@ -1,5 +1,7 @@
 package io.github.tyrleng.karsten.transaction.infrastructure.receive;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,6 +12,8 @@ import io.github.tyrleng.karsten.transaction.domain.incoming.CreateTransactionCo
 import io.github.tyrleng.karsten.transaction.domain.entity.Account;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.io.StringWriter;
 
 /**
  * Reads Json and parses the Json into a command / query.
@@ -34,31 +38,81 @@ public class RequestReceiver {
         try {
             ObjectMapper mapper = new ObjectMapper();
             JsonNode rootNode = mapper.readTree(message);
+            JsonNode requestTypeNode = rootNode.get("requestType");
+            String requestType = requestTypeNode.asText();
 
-            String commandString = rootNode.get("command").asText();
-            switch (commandString) {
-                case "createTransaction":
-                    CreateTransactionCommand command =  new CreateTransactionCommand();
-                    ArrayNode creditArray = (ArrayNode)rootNode.get("credit");
-                    ArrayNode debitArray = (ArrayNode)rootNode.get("debit");
-                    for (JsonNode creditAccountAndAmount : creditArray) {
-                        Account account = new Account(creditAccountAndAmount.get("account").asInt());
-                        JsonNode moneyJsonNode = creditAccountAndAmount.get("money");
-                        Money amount = new Money(moneyJsonNode.get("currencyCode").asText(), moneyJsonNode.get("amount").asText());
-                        command.addCreditAccountAndAmount(account,amount);
-                    }
-                    for (JsonNode debitAccountAndAmount : debitArray) {
-                        Account account = new Account(debitAccountAndAmount.get("account").asInt());
-                        JsonNode moneyJsonNode = debitAccountAndAmount.get("money");
-                        Money amount = new Money(moneyJsonNode.get("currencyCode").asText(), moneyJsonNode.get("amount").asText());
-                        command.addDebitAccountAndAmount(account,amount);
-                    }
-                    return transactionService.createTransaction(command);
+            switch (requestType) {
+                case "query": {
+                    return handleQuery(rootNode);
+                }
+                case "command": {
+                    handleCommand(rootNode);
+                }
             }
-        }
-        catch (JsonProcessingException e) {
+        } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        return "incoming request error";
+        return " ";
+    }
+
+    private String handleQuery (JsonNode rootNode){
+       return null;
+    }
+
+    private String handleCommand (JsonNode rootNode) {
+        JsonNode topicNode = rootNode.get("topic");
+        String topic = topicNode.asText();
+
+        JsonNode requestIdNode = rootNode.get("requestId");
+        int requestId = requestIdNode.asInt();
+
+        switch (topic) {
+            case "createTransaction" : {
+                CreateTransactionCommand command = createTransactionCommand(rootNode);
+                transactionService.createTransaction(command);
+                return createTransactionJson(requestId, topic);
+            }
+        }
+        return "Receiver's Handle Command Fucked Up";
+    }
+
+    private CreateTransactionCommand createTransactionCommand(JsonNode rootNode) {
+        CreateTransactionCommand command =  new CreateTransactionCommand();
+        ArrayNode creditArray = (ArrayNode)rootNode.get("credit");
+        ArrayNode debitArray = (ArrayNode)rootNode.get("debit");
+        for (JsonNode creditAccountAndAmount : creditArray) {
+            Account account = new Account(creditAccountAndAmount.get("account").asInt());
+            JsonNode moneyJsonNode = creditAccountAndAmount.get("money");
+            Money amount = new Money(moneyJsonNode.get("currencyCode").asText(), moneyJsonNode.get("amount").asText());
+            command.addCreditAccountAndAmount(account,amount);
+        }
+        for (JsonNode debitAccountAndAmount : debitArray) {
+            Account account = new Account(debitAccountAndAmount.get("account").asInt());
+            JsonNode moneyJsonNode = debitAccountAndAmount.get("money");
+            Money amount = new Money(moneyJsonNode.get("currencyCode").asText(), moneyJsonNode.get("amount").asText());
+            command.addDebitAccountAndAmount(account,amount);
+        }
+        return command;
+    }
+
+    private String createTransactionJson (int requestId, String topic) {
+        try {
+            JsonFactory jsonFactory = new JsonFactory();
+            StringWriter writer = new StringWriter();
+            JsonGenerator jsonGenerator = jsonFactory.createGenerator(writer);
+            jsonGenerator.useDefaultPrettyPrinter();
+
+            jsonGenerator.writeStartObject();
+            jsonGenerator.writeNumberField("requestId", requestId);
+            jsonGenerator.writeStringField("topic", topic);
+
+            jsonGenerator.writeEndObject();
+            jsonGenerator.close();
+            return writer.toString();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return " ";
     }
 }
